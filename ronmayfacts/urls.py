b305c033a2806a37b76ade54cbad1edcb338e82a
Tim Saylor <tim.saylor@gmail.com>
2012-09-22 20:39:26 -0500

    add static file serving to urls

--- ronmayfacts/urls.py
+++ ronmayfacts/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -16,3 +17,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('facts.urls')),
 )
+
+if not settings.DEBUG:
+    urlpatterns = patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    ) + urlpatterns
