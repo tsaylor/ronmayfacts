150a05e59f189db2be621aad1d59affa68d071bd
Tim Saylor <tim.saylor@gmail.com>
2012-09-22 20:47:05 -0500

    trying a different static root

--- ronmayfacts/settings.py
+++ ronmayfacts/settings.py
@@ -56,7 +56,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.abspath(os.path.join(os.getcwd(), '..', 'static'))
+print STATIC_ROOT
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
