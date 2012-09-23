87ba4a313c2a1acf38690cedc34c84e7327f19a8
Tim Saylor <tim.saylor@gmail.com>
2012-09-22 21:01:05 -0500

    hardcoded like a php programmer

--- ronmayfacts/settings.py
+++ ronmayfacts/settings.py
@@ -56,8 +56,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.abspath(os.path.join(os.getcwd(), '..', 'static'))
-print STATIC_ROOT
+STATIC_ROOT = "/app/static" #os.path.abspath(os.path.join(os.getcwd(), '..', 'static'))
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
