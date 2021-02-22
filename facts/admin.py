eda3102bc09a8facab2c62df580e276a0b87f288
Tim Saylor <tim.saylor@gmail.com>
2021-02-21 23:30:56 -0600

    simple JS version, deployed on s3

--- facts/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from facts.models import Fact
-
-admin.site.register(Fact)
--- facts/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class NewFactForm(forms.Form):
-    name = forms.CharField(max_length="50")
-    email = forms.EmailField()
-    factoid = forms.CharField()
